929eef9266143fa56489b6b83e4bd58917343c54
Gabriel Junior de Souza Dias <gabriel.dias0223@gmail.com>
2022-05-24 05:19:17 -0300

    Fix menu borders

--- Assets/_Project/Scripts/UI/PlayerUIController.cs
+++ Assets/_Project/Scripts/UI/PlayerUIController.cs
@@ -39,13 +39,12 @@ public class PlayerUIController : MonoBehaviour
 
     private void PlayTakeDamageAnimation()
     {
+        #if !UNITY_WEBGL
         Handheld.Vibrate();
-        
+        #endif
+
         Material mat = _playerMachine.GetComponent<Image>().material;
 
-        
-        
-        
         Sequence damageSequence = DOTween.Sequence();
         damageSequence.Append(_damagePanel.DOColor(new Color(_damagePanel.color.r, _damagePanel.color.g, _damagePanel.color.b, 0.5f), 0.25f));
         damageSequence.Join(DOVirtual.Float(0, 0.2f, 0.5f, value => mat.SetFloat("_ChromAberrAmount", value)));
